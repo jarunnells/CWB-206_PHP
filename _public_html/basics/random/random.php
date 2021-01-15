6a300b7011de3e0c58d5ffb77904feae58ac46da
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2021-01-15 02:52:48 -0700

    comment updated

--- _public_html/basics/random/random.php
+++ _public_html/basics/random/random.php
@@ -3,6 +3,8 @@
        COURSE: CIS-206-R11
        BRANCH: main
          FILE: random.php
+
+    [ X ] COMPLETE?
 -->
 <?php
     define('NAV_LINK', '../basics.php');
@@ -54,31 +56,29 @@
         <link rel="stylesheet" href="../styles/custom.css" />
         <title>Random Numbers | PHP</title>
     </head>
-    <body>
         <!--
-        <form action="">
-            <label for="algorithms">Algorithm:</label>
+        <section>
+            Algorithm:
             <select id="algorithms">
                 <option value=<?php //echo RandomNumbers::ALGORITHMS[0][0] ?> selected><?php //echo RandomNumbers::ALGORITHMS[1][0] ?></option>
                 <option value=<?php //echo RandomNumbers::ALGORITHMS[0][1] ?>><?php //echo RandomNumbers::ALGORITHMS[1][1] ?></option>
                 <option value=<?php //echo RandomNumbers::ALGORITHMS[0][2] ?>><?php //echo RandomNumbers::ALGORITHMS[1][2] ?></option>
                 <option value=<?php //echo RandomNumbers::ALGORITHMS[0][3] ?>><?php //echo RandomNumbers::ALGORITHMS[1][3] ?></option>
             </select>
-        </form>
+        </section>
         -->
-        <?php $random_numbers = new RandomNumbers(
-            $algorithm=RandomNumbers::ALGORITHMS[1][random_int(1,count(RandomNumbers::ALGORITHMS[1])-1)]);
-            echo '<section id="holder">';
-            echo '<h3 id="random_title">' . $random_numbers->header . '</h3>';
-            echo '<hr class="title" />';
-            foreach ($random_numbers->generate_numbers() as $number) {
-                echo '<p class="number">' . $number . '</p>';
-            }
-            echo '<hr class="algorithm" />';
-            echo '<p id="algorithm_used">Algorithm Used: '. $random_numbers->algorithm .'()</p>';
-            echo '<hr class="algorithm" />';
-            echo '<a href=' . NAV_LINK . ' class="nav_link" target="_self" title="Go To -> basics.php...">' . BACK . '</a>';
-            echo '</section>';
-        ?>
-    </body>
+    <?php $random_numbers = new RandomNumbers(
+        $algorithm=RandomNumbers::ALGORITHMS[1][random_int(1,count(RandomNumbers::ALGORITHMS[1])-1)]);
+        echo '<body><section id="holder">';
+        echo '<h3 id="random_title">' . $random_numbers->header . '</h3>';
+        echo '<hr class="title" />';
+        foreach ($random_numbers->generate_numbers() as $number) {
+            echo '<p class="number">' . $number . '</p>';
+        }
+        echo '<hr class="algorithm" />';
+        echo '<p id="algorithm_used">Algorithm Used: '. $random_numbers->algorithm .'()</p>';
+        echo '<hr class="algorithm" />';
+        echo '<a href=' . NAV_LINK . ' class="nav_link" target="_self" title="Go To -> basics.php...">' . BACK . '</a>';
+        echo '</section></body>';
+    ?>
 </html>
