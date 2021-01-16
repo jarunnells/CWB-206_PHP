3085a24e9e194310b625e8ec37dd773dcca93164
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2021-01-16 04:03:50 -0700

    cont'd form dev

--- _public_html/basics/form_proj/form_process.php
+++ _public_html/basics/form_proj/form_process.php
@@ -13,11 +13,11 @@
     define('NAV_LINK', '../basics.php');
     define('BACK', '<< BACK >>');
     define('OUTPUT', '<< OUTPUT >>');
+    define('TITLE', 'Sign Up Now');
 
     // VARS
     $br = "<br />";
     $hr = "<br /><hr /><br />";
-    $title_form = 'Sign Up Now';
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -26,15 +26,55 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../styles/basics.css" />
         <link rel="stylesheet" href="../styles/custom.css" />
-        <title>Info Output | PHP</title>
+        <title>Form Process | PHP</title>
     </head>
     <?php
         echo '<body><section id="holder">';
-        echo '<h3 id="form_title">' . $title_form . '</h3>';
+        echo '<h3 id="form_title">' . TITLE . '</h3>';
         echo '<hr class="title" />';
-        echo '<a href=' . NAV_LINK . ' class="nav_link" target="_self" title="Go To -> basics.php...">' . BACK . '</a>';
+    ?>
+        <form action="info_output.php" method="get" id="form_process">
+            <table>
+                <caption>Player Info</caption>
+                <tr>
+                    <td><label for="fname">First Name:</label></td>
+                    <td><input type="text" name="fname" id="fname"></td>
+                </tr>
+                <tr>
+                    <td><label for="lname">Last Name:</label></td>
+                    <td><input type="text" name="lname" id="lname"></td>
+                </tr>
+                <tr>
+                    <td><label for="address">Address:</label></td>
+                    <td><input type="text" name="address" id="address"></td>
+                </tr>
+                <tr>
+                    <td><label for="city">City:</label></td>
+                    <td><input type="text" name="city" id="city"></td>
+                </tr>
+                <tr>
+                    <td><label for="state">State:</label></td>
+                    <td><input type="text" name="state" id="state"></td>
+                </tr>
+                <tr>
+                    <td><label for="zip">Zip:</label></td>
+                    <td><input type="text" name="zip" id="zip"></td>
+                </tr>
+                <tr>
+                    <td><label for="phone">Phone:</label></td>
+                    <td><input type="text" name="phone" id="phone"></td>
+                </tr>
+                <tr>
+                    <td><button type="submit">SUBMIT</button></td>
+                    <td><button type="reset">CLEAR</button></td>
+                </tr>
+            </table>
+        </form>
+    <?php
+        echo '<hr class="title" />';
+        echo '<a href=' . NAV_OUTPUT . ' class="nav_link" target="_self" title="Go To -> info_output.php...">' . OUTPUT . '</a>';        
         echo $br;
-        echo '<a href=' . NAV_OUTPUT . ' class="nav_link" target="_self" title="Go To -> info_output.php...">' . OUTPUT . '</a>';
+        echo '<a href=' . NAV_LINK . ' class="nav_link" target="_self" title="Go To -> basics.php...">' . BACK . '</a>';
         echo '</section></body>';
     ?>
 </html>
--- _public_html/basics/form_proj/info_output.php
+++ _public_html/basics/form_proj/info_output.php
@@ -21,7 +21,11 @@
 
     // INSTANTIATE
     $player = new Player('Ziggy', 'Wiggy');
+    $player->set_address('1234 North Main Street', 'AnyTown', 'CA', '90210');
+    $player->set_phone('415.555.1234');
     $player_minor = new MinorPlayer('Ziggy', 'Wiggy', '05/26/2016', 'Marley', 'McFarley');
+    $player_minor->set_address('4321 South Main Street', 'AnyTown', 'CO', '80642');
+    $player_minor->set_phone('720.555.4321');
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -39,8 +43,12 @@
         echo "Player(asString=true): " . $player->get_player_name(true) . $br;
         echo "Player['first']: " . $player->get_player_name()['first'] . $br;
         echo "Player['last']: " . $player->get_player_name()['last'] . $br;
-        echo $hr;
-        echo "MinorPlayer->Parent(asString=true): " . $player_minor->get_parent_name(true) . $br;
+        echo "Address['street']: " . $player->get_address()['street'] . $br;
+        echo "Address['city']: " . $player->get_address()['city'] . $br;
+        echo "Address['state']: " . $player->get_address()['state'] . $br;
+        echo "Address['zip']: " . $player->get_address()['zip'] . $br;
+        echo "Phone: " . $player->get_phone() . $br;
+        echo '<hr class="title" />';
         echo '<a href=' . NAV_LINK . ' class="nav_link" target="_self" title="Go To -> basics.php...">' . BACK . '</a>';
         echo '</section></body>';
     ?>
