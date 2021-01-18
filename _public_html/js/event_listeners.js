75cf07a60b71744050bb0070fab59ebcd4c0d808
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2021-01-17 22:01:13 -0700

    adding origin -> commit before sync

--- _public_html/js/event_listeners.js
+++ _public_html/js/event_listeners.js
@@ -1,12 +1,22 @@
 "use strict";
 
+/*
+    DEVELOPER: J.A. Runnells
+       COURSE: CIS-206-R11
+       BRANCH: main
+         FILE: event_listeners.js
+
+*/
+
 // VARS
 let hamburger = document.querySelector(".hamburger"),
-    lbl_hamburger = document.querySelector(".hamburger-label");
+    lbl_hamburger = document.querySelector(".hamburger-label"),
+    nav_menu = document.querySelector("#nav_menu");
 // EVENT -> OnClick
 hamburger.addEventListener("click", () => {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     lbl_hamburger.classList.toggle("is-active");
+    nav_menu.classList.toggle("is-active");
     // Do something else, like open/close menu
 });
