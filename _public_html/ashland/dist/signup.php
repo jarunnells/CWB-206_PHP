2a180ceb5dc7fd8184d104c5df5d8397b6dbd09f
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2021-03-09 05:05:09 -0700

    removed required attr from input flds

--- _public_html/ashland/dist/signup.php
+++ _public_html/ashland/dist/signup.php
@@ -51,43 +51,43 @@
             <tbody>
               <tr>
                 <td><label for="player_fname">Player First Name</label></td>
-                <td><input type="text" id="player_fname" name="pla_fname" maxlength="25" value="<?php echo sanitize_input($pla_fname); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="player_fname" name="pla_fname" maxlength="25" value="<?php echo sanitize_input($pla_fname); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="player_lname">Player Last Name</label></td>
-                <td><input type="text" id="player_lname" name="pla_lname" maxlength="25" value="<?php echo sanitize_input($pla_lname); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="player_lname" name="pla_lname" maxlength="25" value="<?php echo sanitize_input($pla_lname); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="player_bday">Player Birth Date</label></td>
-                <td><input type="date" id="player_bday" name="pla_bdate" min="<?php /* @var $cal_min */ echo $cal_min; ?>" max="<?php /* @var $cal_max */ echo $cal_max; ?>" placeholder="<?php echo PLACEHOLDERS['bday'] ?>" title="<?php echo TITLES['bday'] ?>" value="<?php echo sanitize_input($pla_bdate); ?>" ><span class="required">*</span></td>
+                <td><input type="date" id="player_bday" name="pla_bdate" min="<?php /* @var $cal_min */ echo $cal_min; ?>" max="<?php /* @var $cal_max */ echo $cal_max; ?>" placeholder="<?php echo PLACEHOLDERS['bday'] ?>" title="<?php echo TITLES['bday'] ?>" value="<?php echo sanitize_input($pla_bdate); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="player_street">Street Address</label></td>
-                <td><input type="text" id="player_street" name="pla_add" maxlength="50" value="<?php echo sanitize_input($pla_add); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="player_street" name="pla_add" maxlength="50" value="<?php echo sanitize_input($pla_add); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="player_city">City</label></td>
-                <td><input type="text" id="player_city" name="pla_city" maxlength="50" value="<?php echo sanitize_input($pla_city); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="player_city" name="pla_city" maxlength="50" value="<?php echo sanitize_input($pla_city); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="player_state">State</label></td>
-                <td><input type="text" id="player_state" name="pla_state" placeholder="<?php echo PLACEHOLDERS['state'] ?>" title="<?php echo TITLES['state'] ?>" maxlength="2" value="<?php echo sanitize_input($pla_state); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="player_state" name="pla_state" placeholder="<?php echo PLACEHOLDERS['state'] ?>" title="<?php echo TITLES['state'] ?>" maxlength="2" value="<?php echo sanitize_input($pla_state); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="player_zip">Zipcode</label></td>
-                <td><input type="text" id="player_zip" name="pla_zip" placeholder="<?php echo PLACEHOLDERS['zip'] ?>" title="<?php echo TITLES['zip'] ?>" minlength="5" maxlength="10" value="<?php echo sanitize_input($pla_zip); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="player_zip" name="pla_zip" placeholder="<?php echo PLACEHOLDERS['zip'] ?>" title="<?php echo TITLES['zip'] ?>" minlength="5" maxlength="10" value="<?php echo sanitize_input($pla_zip); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="player_phone">Primary Phone</label></td>
-                <td><input type="text" id="player_phone" name="pla_phone" placeholder="<?php echo PLACEHOLDERS['phone'] ?>" title="<?php echo TITLES['phone'] ?>" maxlength="14" value="<?php echo sanitize_input($pla_phone); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="player_phone" name="pla_phone" placeholder="<?php echo PLACEHOLDERS['phone'] ?>" title="<?php echo TITLES['phone'] ?>" maxlength="14" value="<?php echo sanitize_input($pla_phone); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="parent_fname">Parent First Name</label></td>
-                <td><input type="text" id="parent_fname" name="pla_par_fname" maxlength="25" value="<?php echo sanitize_input($pla_par_fname); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="parent_fname" name="pla_par_fname" maxlength="25" value="<?php echo sanitize_input($pla_par_fname); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <td><label for="parent_lname">Parent Last Name</label></td>
-                <td><input type="text" id="parent_lname" name="pla_par_lname" maxlength="25" value="<?php echo sanitize_input($pla_par_lname); ?>" ><span class="required">*</span></td>
+                <td><input type="text" id="parent_lname" name="pla_par_lname" maxlength="25" value="<?php echo sanitize_input($pla_par_lname); ?>"><span class="required">*</span></td>
               </tr>
               <tr>
                 <th id="required" colspan="2">* Required</th>
